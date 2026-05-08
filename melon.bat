9600e5a86043adcf1e5d344d1ad2cc88cf053845
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-05-08 18:04:44 +0200

    Added new batch command to run exectuable

--- melon.bat
+++ melon.bat
@@ -6,11 +6,14 @@ set BUILD_DIR=build
 if "%1"=="build" goto build
 if "%1"=="check" goto check
 if "%1"=="clean" goto clean
+if "%1"=="run"   goto run
+
 
 echo Usage:
 echo   melon build   - build + run
 echo   melon check   - just compile
 echo   melon clean   - delete build folder
+echo   melon run     - run Sandbox
 exit /b
 
 :check
@@ -33,4 +36,9 @@ exit /b
 :clean
 echo [Melon] Cleaning build folder...
 rmdir /s /q %BUILD_DIR%
+exit /b
+
+:run
+echo [Melon] Running Sandbox...
+%BUILD_DIR%\Sandbox\Debug\Sandbox.exe
 exit /b
