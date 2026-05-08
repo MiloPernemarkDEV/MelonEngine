0b6f226620c038d128eca9b8c5e19c2e7046780a
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-05-08 16:44:52 +0200

    Initial commit

---
+++ Engine/src/Engine.cpp
@@ -0,0 +1,10 @@
+//
+// Created by milos on 2026-05-08.
+//
+
+#include <iostream>
+
+void EngineTest()
+{
+    std::cout << "Engine is alive\n";
+}
---
+++ melon.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal
+
+set BUILD_DIR=build
+
+if "%1"=="build" goto build
+if "%1"=="check" goto check
+if "%1"=="clean" goto clean
+
+echo Usage:
+echo   melon build   - build + run
+echo   melon check   - just compile
+echo   melon clean   - delete build folder
+exit /b
+
+:check
+echo [Melon] Configuring project...
+cmake -S . -B %BUILD_DIR%
+echo [Melon] Building...
+cmake --build %BUILD_DIR%
+exit /b
+
+:build
+echo [Melon] Configuring project...
+cmake -S . -B %BUILD_DIR%
+echo [Melon] Building...
+cmake --build %BUILD_DIR%
+
+echo [Melon] Running Sandbox...
+%BUILD_DIR%\Sandbox\Debug\Sandbox.exe
+exit /b
+
+:clean
+echo [Melon] Cleaning build folder...
+rmdir /s /q %BUILD_DIR%
+exit /b
