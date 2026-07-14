54914d550f916ea5b1982b04bf0f9e1d5df74a45
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-07-14 16:22:21 +0200

    Add generic bsp node struct with traverse method for binary space partitioning

---
+++ engine/core/public/bsp_tree.h
@@ -0,0 +1,30 @@
+#ifndef MELON_BSP_TREE_H
+#define MELON_BSP_TREE_H
+
+#include <functional>
+#include "types.h"
+
+template<typename T>
+struct BspNode {
+    T data;
+    BspNode* leftChild;
+    BspNode* rightChild;
+
+    bool isLeaf = true;
+
+    void Traverse(std::function<int(BspNode<T>*)> visitor) {
+        s32 decision = visitor(this);
+
+        if (isLeaf || decision == 0) {
+            return;
+        }
+        if (decision == 1 && leftChild) {
+            leftChild->Traverse(visitor);
+        }
+        if (decision == 2 && rightChild) {
+            rightChild->Traverse(visitor);
+        }
+    }
+};
+
+#endif // MELON_BSP_TREE_H
